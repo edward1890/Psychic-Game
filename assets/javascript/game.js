07c84429a8a9c0d97ecc57ca34733080b9f4a7f1
ed refvem <edrefvem@eds-MacBook-Pro.local>
2018-02-18 23:15:13 -0700

    psychic-game

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+document.onekeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+    
+    if (userGuess == computerGuess) {
+        wins++;
+        alert('Way to go! You Won!');
+        guessesLeft = 9;
+        guessesSoFar.length = 0; 
+        console.log('you won')
+    }
+    else if (guessesLeft == 0){
+        losses++;
+        alert('You lost. try again.');
+        guessesLeft = 9;
+        guessesSoFar.length = 0;
+        console.log('you lost')
+    }
+    else if (userGuess !== computerGuess){
+        guessesLeft--;
+    }  
+
+    document.querySelector('#game').innerHTML = html;
+    var html = "<h1>The Psychic Game</h1>" + 
+    "<p>Guess what letter I'm thinking of!</p>" +
+    "<p>Total Wins: " + 
+    wins + 
+    "</p>" +
+    "<p>Total Losses: " + 
+    losses + 
+    "</p>" +
+    "<p>Guesses Left: " + 
+    guessesLeft + 
+    "</p>" +
+    "<p>Your Guesses so far: " +
+    guessesSoFar +
+    "</p>"
+    ;
+    
+
+}
